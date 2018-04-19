dd198a46b4938e4056b3b2eae4bf775ba0c2e83d
pkatterjohn <pkatterjohn@gmail.com>
2018-04-19 16:00:05 -0500

    Logging query to console and responding with fields'

--- backend/server.js
+++ backend/server.js
@@ -20,30 +20,35 @@ app.get('/', function(req,res){
 
 app.get('/items',function(req,res){
 client.query('SELECT * FROM items', (err,response) => {
-    res.send('Items Page\n' + JSON.stringify(response.fields));
+    	console.log(response);
+    	res.send('Items Page\n' + JSON.stringify(response.fields));
 });
 });
 
 app.get('/characters',function(req,res){
 client.query('SELECT * FROM characters', (err,response) => {
+    	console.log(response);
 	res.send('Characters Page\n' + JSON.stringify(response.fields));
 });
 });
 
 app.get('/backpacks',function(req,res){
 client.query('SELECT * FROM backpacks', (err,response) => {
+    	console.log(response);
 	res.send('Backpacks Page\n' + JSON.stringify(response.fields));
 });
 });
 
 app.get('/spells',function(req,res){
 client.query('SELECT * FROM spells', (err,response) => {
+    	console.log(response);
 	res.send('Spells Page\n' + JSON.stringify(response.fields));
 });
 });
 
 app.get('/races',function(req,res){
 client.query('SELECT * FROM races', (err,response) => {
+    	console.log(response);
 	res.send('Races Page\n' + JSON.stringify(response.fields));
 });
 });
@@ -51,12 +56,14 @@ client.query('SELECT * FROM races', (err,response) => {
 
 app.get('/classes',function(req,res){
 client.query('SELECT * FROM classes', (err,response) => {
+    	console.log(response);
 	res.send('Classes Page\n' + JSON.stringify(response.fields));
 });
 });
 
 app.get('/equipped',function(req,res){
 client.query('SELECT * FROM equipped', (err,response) => {
+    	console.log(response);
 	res.send('Equipped Page\n'+ JSON.stringify(response.fields));
 });
 });
