e9e5df51496a1bc2927bce30be23d6c20923fe38
Wilson Ding <hello@wilsonding.com>
2018-04-21 14:44:57 -0500

    Added DetailedCharacterViewController

---
+++ iOS/RPG DB/RPG DB/Controllers/DetailedCharacterViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailedCharacterViewController.swift
+//  RPG DB
+//
+//  Created by Wilson Ding on 4/21/18.
+//  Copyright © 2018 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class DetailedCharacterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
