b25e40b14c25c47db0f8274e61eb9988693b9b3f
Wilson Ding <hello@wilsonding.com>
2018-04-21 14:35:35 -0500

    Initial file structure

---
+++ iOS/RPG DB/RPG DB/Models/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  RPG DB
+//
+//  Created by Wilson Ding on 4/21/18.
+//  Copyright © 2018 Wilson Ding. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+}
---
+++ iOS/RPG DB/RPG DB/Views/CharacterTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CharacterTableViewCell.swift
+//  RPG DB
+//
+//  Created by Wilson Ding on 4/21/18.
+//  Copyright © 2018 Wilson Ding. All rights reserved.
+//
+
+import UIKit
+
+class CharacterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
