08fcaa5ecad5f4bbd70eb776be4576046a303284
U-CAMPUS\pak140030 <pak140030@spnc254440.campus.ad.utdallas.edu>
2018-04-23 12:59:07 -0500

    Added logger to server.js and expanded queries, need to finish documenting README file but i have documented all the code in the server.js file

--- backend/server.js
+++ backend/server.js
@@ -11,59 +11,173 @@ const client = new Client({
 	port: 5432
 });
 client.connect();
+
+
+//This is the homepage for our API, nothing really going on here just a casual rest to ensure connectivity
 app.get('/', function(req,res){
-	console.log('Home');
-	res.write('Hello World! :D');
+	logger('Endpoint: Homepage');
+	res.write('Welcome to the awesome homepage that is our RPG Database project xD');
 	res.end();
-
 });
-
+/*
+--------------------------------------------------------
+Endpoint for items, '/items' returns all items in  our
+database while '/items/:id' will return item with :id
+--------------------------------------------------------
+*/
 app.get('/items',function(req,res){
-client.query("SELECT * FROM items WHERE item_type='hand'", (err,response) => {
-    	console.log(response);
+client.query(logger("SELECT * FROM items"), (err,response) => {
+	//logger(response);
     	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/items/:id', function(req,res){
+let sql = 'SELECT * FROM items WHERE item_id=' + req.params.id;
+client.query(logger(sql), (err,response) => {
+	//logger(response);
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+/*
+--------------------------------------------------------
+Character Endpoint
+--------------------------------------------------------
+*/
 app.get('/characters',function(req,res){
-client.query('SELECT * FROM characters', (err,response) => {
-    	console.log(response);
+client.query(logger('SELECT * FROM characters'), (err,response) => {
+	//logger(response);
 	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/characters/:id', function(req,res){
+let sql = 'SELECT * FROM characters WHERE char_id=' + req.params.id;
+client.query(logger(sql), (err,response) => {
+	//logger(response);
+	res.send(JSON.stringify(response.rows));
+});
+});
+/*
+--------------------------------------------------------
+Backpack Endpoint
+'/backpack/:id' returns a characters backpack
+--------------------------------------------------------
+*/
 app.get('/backpacks',function(req,res){
-client.query('SELECT * FROM backpacks', (err,response) => {
-    	console.log(response);
+client.query(logger('SELECT * FROM backpacks'), (err,response) => {
+    	//logger(response);
 	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/backpacks/:id',function(req,res){
+let sql = 'SELECT * FROM backpacks WHERE char_id=' + req.params.id;
+client.query(logger(sql), (err,response) => {
+	//logger(response);
+	res.send(JSON.stringify(response.rows));
+});
+});
+/*
+--------------------------------------------------------
+Spell Endpoint
+'/spells/class/id' returns all spells for class with id
+'/spells/race/id' returns all spells for class with id
+--------------------------------------------------------
+*/
 app.get('/spells',function(req,res){
-client.query('SELECT * FROM spells', (err,response) => {
-    	console.log(response);
+client.query(logger('SELECT * FROM spells'), (err,response) => {
+    	//logger(response);
 	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/spells/:id', function(req,res){
+let sql = 'SELECT * FROM spells WHERE spell_id='+req.params.id;
+client.query(logger(sql), (err, response) => {
+	//logger(response)
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+app.get('/spells/class/:id', function(req,res){
+let sql = 'SELECT * FROM spells where class_id='+req.params.id;
+client.query(logger(sql), (err, response) => {
+	//logger(response)
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+
+app.get('/spells/race/:id', function(req,res){
+let sql = 'SELECT * FROM spells where race_id='+req.params.id;
+client.query(logger(sql), (err, response) => {
+	//logger(response)
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+/*
+--------------------------------------------------------
+Race Endpoint
+'/races/:id' returns all characters that are of race id
+--------------------------------------------------------
+*/
 app.get('/races',function(req,res){
-client.query('SELECT * FROM races', (err,response) => {
+client.query(logger('SELECT * FROM races'), (err,response) => {
     	console.log(response);
 	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/races/:id', function(req,res){
+let sql = 'SELECT * FROM characters where race_id='+req.params.id;
+client.query(logger(sql), (err, response) => {
+	//logger(response)
+	res.send(JSON.stringify(response.rows));
+});
+});
 
+
+/*
+----------------------------------------------------------
+Class Endpoint
+'/classes/:id' returns all characters that are of class id
+----------------------------------------------------------
+*/
 app.get('/classes',function(req,res){
-client.query('SELECT * FROM classes', (err,response) => {
-    	console.log(response);
+client.query(logger('SELECT * FROM classes'), (err,response) => {
+    	//logger(response);
 	res.send(JSON.stringify(response.rows));
 });
 });
 
+app.get('/classes/:id', function(req,res){
+let sql = 'SELECT * FROM characters WHERE class_id='+req.params.id;
+client.query(logger(sql), (err,response)=>{
+	//logger(response);
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+/*
+--------------------------------------------------------
+Equipped Endpoint
+'/equipped/:id' returns equipped items for character
+--------------------------------------------------------
+*/
 app.get('/equipped',function(req,res){
-client.query('SELECT * FROM equipped', (err,response) => {
-    	console.log(response);
+client.query(logger('SELECT * FROM equipped'), (err,response) => {
+    	//logger(response);
+	res.send(JSON.stringify(response.rows));
+});
+});
+
+app.get('/equipped/:id', function(req,res){
+let sql = 'SELECT * FROM equipped WHERE char_id='+req.params.id;
+client.query(logger(sql), (err, response) => {
+	//logger(response)
 	res.send(JSON.stringify(response.rows));
 });
 });
@@ -71,3 +185,12 @@ client.query('SELECT * FROM equipped', (err,response) => {
 var server = app.listen(3000, function() {
     console.log('Express is listening to http://localhost:3000');
 });
+
+function logger(data){
+d = new Date();
+console.log("---------------------------------------------------------");
+console.log(d.toLocaleString());
+console.log(data);
+return data;
+};
+
